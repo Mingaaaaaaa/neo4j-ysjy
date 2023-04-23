c5acd1ac59dc6e1e6235056cb91a7c7e7d2b9232
Alplune <mingaaaa@outlook.com>
2023-04-24 00:14:18 +0800

    aaaaaaa

--- src/components/App.js
+++ src/components/App.js
@@ -15,11 +15,11 @@ const App = () => {
     useEffect(() => {
         const config = {
             container_id: 'id1',
-            server_url: "neo4j+s://124.222.4.79",
+            server_url: "bolt://124.222.4.79",
             server_user: "neo4j",
             server_password: "password",
             driverConfig: { 
-                // encrypted: "ENCRYPTION_ON",
+                 encrypted: "ENCRYPTION_ON",
                 trust: "TRUST_SYSTEM_CA_SIGNED_CERTIFICATES"
                 },
             labels: {
